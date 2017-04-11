6fcd9eb55ce27570dd145a464860c967ec1be047
nilesh9131 <nilesh9131@gmail.com>
2017-04-11 16:05:20 +0530

    First commit,annote

--- app/src/main/java/com/neel/fcmstorage/MainActivity.java
+++ app/src/main/java/com/neel/fcmstorage/MainActivity.java
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity  implements  View.OnClickLis
     public void getDownlaodfile()
     {
         try {
-            //neel
+            //neelesh
             final File localFile = File.createTempFile("images", "jpg");
             mStorageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                 @Override
